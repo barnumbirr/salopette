527a42000d411a802fa18efb3c1c75fb3cca513a
Martin Simon <martin@tentwentyfour.lu>
2015-01-22 09:48:14 +0100

    first commit

---
+++ doc/README.md
@@ -0,0 +1,6 @@
+Icon made by [Freepik][] from [www.flaticon.com][] is licensed under [CC
+BY 3.0][].
+
+[Freepik]: http://www.freepik.com "Freepik"
+[www.flaticon.com]: http://www.flaticon.com "Flaticon"
+[CC BY 3.0]: http://creativecommons.org/licenses/by/3.0/
---
+++ salopette/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#             _                  _   _       
+#   ___  __ _| | ___  _ __   ___| |_| |_ ___ 
+#  / __|/ _` | |/ _ \| '_ \ / _ \ __| __/ _ \
+#  \__ \ (_| | | (_) | |_) |  __/ |_| ||  __/
+#  |___/\__,_|_|\___/| .__/ \___|\__|\__\___|
+#                    |_|                     
+
+
+__title__   = 'salopette'
+__version__ = '0.1a'
+__author__  = '@c0ding'
+__repo__    = 'https://github.com/c0ding/salopette'
+__license__ = 'Apache v2.0 License'
