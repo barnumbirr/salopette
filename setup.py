891f3441bfa46dd454b84ac14208d7cff8aebd74
Martin Simon <me@martinsimon.me>
2015-04-07 18:18:23 +0200

    register on PyPi

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = 'salopette',
+	version = '0.1a',
+	url = 'https://github.com/c0ding/salopette',
+	download_url = 'https://github.com/c0ding/salopette/archive/master.zip',
+	author = 'Martin Simon',
+	author_email = 'me@martinsimon.me',
+	license = 'Apache v2.0 License',
+	packages = ['salopette'],
+	description = 'salopette is an APACHE licensed library written in Python.',
+	long_description = file('README.md','r').read(),
+	keywords = [''],
+)
